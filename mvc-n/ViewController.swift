b785181052c86ba336570869d75ae3175ac00173
DmrLutfullin <dmr.lutfullin@gmail.com>
2020-01-19 04:32:48 +0500

    Initial Commit

---
+++ mvc-n/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvc-n
+//
+//  Created by Дамир Лутфуллин on 19.01.2020.
+//  Copyright © 2020 Damir Lutfullin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
